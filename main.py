8c27b4a2632b2558a0abf3b81713c2a0a348d11e
Bertoxic <dirusalbert@gmail.com>
2022-06-02 21:45:19 -0700

    update

--- main.py
+++ main.py
@@ -5,33 +5,33 @@ moves = ["R", "P", "S"]
 
 while True:
     computer = moves[randint(0,2)]
-    player = input('Please werey input your choice: ').lower()
-    if player=='end':
+    player = input('Please werey input your choice, S for scissors, P for paper etc: ').upper()
+    if player=='END':
         print('so you don japa?')
         break
-    
+
     elif computer==player:
          print ('Tie')
-    elif player=='rock':
-        if computer=="paper":
+    elif player=='R':
+        if computer=="P":
             print('computer picks:',computer,'player picks: ',player)
             print('you lose ', computer ,'beats', player)
         else:
             print('computer picks:',computer,'player picks: ',player)
             print('you win ', player ,'beats', computer)
-    elif player=="paper":
-       if computer=="Scissors":
+    elif player=="P":
+       if computer=="S":
             print('computer picks:',computer,'player picks: ',player)
             print('you lose ', computer ,'beats', player)
        else:
             print('computer picks:',computer,'player picks: ',player)
             print('you win ', player ,'beats', computer)
-    elif player=="scissors":
-       if computer=="rock":
+    elif player=="S":
+       if computer=="R":
             print('computer picks:',computer,'player picks: ',player)
             print('you lose ', computer ,'beats', player)
        else:
             print('computer picks:',computer,'player picks: ',player)
             print('you win ', player ,'beats', computer)
     else:
-        print('ohmo write correct english na...pick ,R ,S , or ,P')
+        print('ohmo write correct english na...pick ,R ,S , or ,P or end')
